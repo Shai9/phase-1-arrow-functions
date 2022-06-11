f3f3cbead105b2149a162d4a2ec1f8a413b667e8
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-12 01:31:46 +0300

    commit tree 7531d6b2e8ccb965744ae587b4bac191af175875

---
+++ index.js
@@ -0,0 +1,28 @@
+function divide () 
+{ 
+  let num1 = 2000;
+  let num2 = 100
+   return (20);
+
+}
+let square = () => {
+ return (2*2)
+}
+ 
+const add = (a, nb) => {
+  return a+nb
+}
+
+  
+  
+  
+
+
+
+
+
+ 
+
+
+
+
